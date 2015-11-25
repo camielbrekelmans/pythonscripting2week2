7b012bcfc87f71af4ce0e8c2e8c140060801af53
Camiël Brekelmans <camiel.brekelmans@student.hu.nl>
2015-11-25 11:06:53 +0100

    Kleine aanpassing

--- Opdracht6.py
+++ Opdracht6.py
@@ -3,19 +3,19 @@ import numpy as np
 
 
 ###opdracht1
-##plt.plot([1,2,4,9,16,25,36,1])
-##plt.show()
+plt.plot([1,2,4,9,16,25,36,1])
+plt.show()
 
-###opdracht 2+3
-##plt.plot([7,8,9,10,11,12,13,14,15,16,17,18],[10, 10, 10, 30, 20, 25, 30, 35, 70, 75, 80, 10],label='Belasting')
-##plt.axis([7,18,0,100])
-##plt.title('Processorbelasting')
-##plt.xlabel('Uren')
-##plt.ylabel('Belasting (uitgedruikt in gemiddeld % per uur)')
-##plt.legend(loc='upper left')
-##plt.annotate('Wat gebeurt hier?!', xy=(17,80), xytext=(15,90),arrowprops=dict(facecolor='black'))
-##plt.grid(True)
-##plt.show()
+#opdracht 2+3
+plt.plot([7,8,9,10,11,12,13,14,15,16,17,18],[10, 10, 10, 30, 20, 25, 30, 35, 70, 75, 80, 10],label='Belasting')
+plt.axis([7,18,0,100])
+plt.title('Processorbelasting')
+plt.xlabel('Uren')
+plt.ylabel('Belasting (uitgedruikt in gemiddeld % per uur)')
+plt.legend(loc='upper left')
+plt.annotate('Wat gebeurt hier?!', xy=(17,80), xytext=(15,90),arrowprops=dict(facecolor='black'))
+plt.grid(True)
+plt.show()
 
 ##plt.plot([2011,2012,2013,2014,2015], [35.15,50.4,55.55,55.02,53.21], label='Win7')
 ##plt.plot([2011,2012,2013,2014,2015], [43.63,30.47,22.53,15.73,10.51], label='WinXP')
@@ -55,3 +55,5 @@ import numpy as np
 ##plt.legend(loc='upper right')
 ##plt.show()
 ##f.close()
+
+
